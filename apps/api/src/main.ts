7e486d7e7376c25382d3c4293b25228b34c64da2
Jaemin Kim <jay.kimjaemin@gmail.com>
2024-04-20 15:44:27 +0900

    feat: access token 받는 api 작성

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  });
   await app.listen(3000);
 }
 bootstrap();
