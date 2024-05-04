99802e6f83850656f50caedf441dc3f94e9c8b68
Jaemin Kim <jay.kimjaemin@gmail.com>
2024-05-04 16:34:12 +0900

    feat: mypage publishing

--- apps/web/src/activities/ProfileActivity/index.tsx
+++ apps/web/src/activities/ProfileActivity/index.tsx
@@ -1,12 +1,32 @@
 import type { ActivityComponentType } from '@stackflow/react';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { BottomNavigation } from '../../components/BottomNavigation';
-import { Button, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  Input,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Radio,
+  RadioGroup,
+  Typography,
+  styled,
+  useTheme,
+} from '@mui/material';
+
 import { ScreenContainer } from '../../components/Containers/ScreenContainer';
 import { useFlow } from '../../layouts/stackflow';
+import Login from '@mui/icons-material/Login';
+import GavelIcon from '@mui/icons-material/Gavel';
 
 const ProfileActivity: ActivityComponentType = () => {
   const { push } = useFlow();
+  const theme = useTheme();
 
   function handleLogin() {
     push('AuthActivity', {}, { animate: true });
@@ -14,13 +34,78 @@ const ProfileActivity: ActivityComponentType = () => {
 
   return (
     <AppScreen>
-      <ScreenContainer>
-        <Typography variant="h1">Profile Activity</Typography>
-        <Button onClick={handleLogin}>카카오 로그인</Button>
+      <ScreenContainer style={{ padding: 0 }}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="50%"
+          width="100%"
+          sx={{ backgroundColor: theme.colors.primary.lighter }}
+        >
+          <Circle style={{ backgroundImage: `url('https://picsum.photos/52/52')` }} />
+        </Box>
+        <Box px={2} width="100%">
+          <Box display="flex" flexDirection="column" gap={1} m={1} p={2} border="1px solid #ddd">
+            <Typography variant="h6">내 정보 수정</Typography>
+            <FormControl style={{ gap: 10 }}>
+              <RadioGroup
+                aria-labelledby="성별 수정"
+                defaultValue="female"
+                name="gender-group"
+                sx={{ flexDirection: 'row' }}
+              >
+                <FormControlLabel value="female" control={<Radio />} label="여자" />
+                <FormControlLabel value="male" control={<Radio />} label="남자" />
+                <FormControlLabel value="other" control={<Radio />} label="Other" />
+              </RadioGroup>
+              <Input placeholder="생년월일" type="date" />
+              <Input placeholder="체중" type="number" />
+              <Input placeholder="키" type="number" />
+              <Button variant="outlined" color="primary" style={{ marginTop: 10 }}>
+                수정하기
+              </Button>
+            </FormControl>
+          </Box>
+          <List>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <GavelIcon />
+                </ListItemIcon>
+                <ListItemText primary="약관" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton onClick={handleLogin}>
+                <ListItemIcon>
+                  <Login />
+                </ListItemIcon>
+                <ListItemText primary="카카오 로그아웃" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
       </ScreenContainer>
       <BottomNavigation />
     </AppScreen>
   );
 };
+
+const Circle = styled(Box)(({ theme }) => ({
+  borderRadius: '50%',
+  width: 100,
+  height: 100,
+  backgroundColor: theme.colors.primary.main,
+  backgroundSize: 'contain',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: theme.colors.primary.dark,
+  },
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
 
 export default ProfileActivity;
