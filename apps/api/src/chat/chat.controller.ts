188c1ac802aaa424b1d28fbb7cbda48128bb2e1f
beoygnas <rlatkdduq99@gmail.com>
2024-05-18 18:29:05 +0900

    feat: add chat feature

--- apps/api/src/chat/chat.controller.ts
+++ apps/api/src/chat/chat.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { AuthGuard } from '@nestjs/passport';
+import { ChatInferDto } from '@repo/ui/types';
+import { Request, Response } from 'express';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
-  @Get('/:chatId')
-  getChat(@Param('chatId') chatId: number) {
-    return this.chatService.getChat(chatId);
+  @Get('/')
+  @UseGuards(AuthGuard('jwt'))
+  getChatByUserId(@Req() req: Request) {
+    return this.chatService.findChatByUserId(req.user.id);
+  }
+
+  @Post('/infer')
+  @UseGuards(AuthGuard('jwt'))
+  async chatInference(@Req() req: Request, @Res() res: Response, @Body() body: ChatInferDto) {
+    try {
+      const a = await this.chatService.inferChat({ ...body }, req.user.id);
+      console.log(a);
+      // this.chatService.postChat({ ...body });
+      return res.send({ message: 'success' });
+    } catch (err) {
+      return res.send({ message: 'error' });
+    }
   }
 }
--- apps/api/src/chat/chat.service.ts
+++ apps/api/src/chat/chat.service.ts
@@ -2,12 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Chat } from './chat.entity';
+import { ChatInferDto } from '@repo/ui/types';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class ChatService {
-  constructor(@InjectRepository(Chat) private mealRepository: Repository<Chat>) {}
+  constructor(
+    @InjectRepository(Chat) private chatRepository: Repository<Chat>,
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
 
-  getChat(chatId: number) {
-    return this.mealRepository.findOne({ where: { id: chatId } });
+  async findChatByUserId(userId: number) {
+    return this.chatRepository.find({ where: { userId } });
   }
+  async inferChat(chat: ChatInferDto, userId: number) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    const bodyForInference = {
+      chat: chat.chat,
+      day_nutrient: chat.day_nutrient,
+      user_data: {
+        gender: user.gender,
+        age: user.age,
+        height: user.height,
+        weight: user.weight,
+        goal: user.goal,
+      },
+    };
+
+    return bodyForInference;
+  }
+  // async postChat(chat: ) {
+
+  // }
 }
