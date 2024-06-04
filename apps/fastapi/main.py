c821404aaf288c7b8a8774deeaf7aac7b608dda4
beoygnas <rlatkdduq99@gmail.com>
2024-06-04 20:38:38 +0900

    feat: apply exception to fastAPI server

--- apps/fastapi/main.py
+++ apps/fastapi/main.py
@@ -1,7 +1,7 @@
 import os, sys
 import json
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 from datetime import date
@@ -50,17 +50,17 @@ async def meal_infer(item: MealInferItem):
     image = item['image']
     food_category = item['food_category']
     user_data = json.dumps(dict(item['user_data']), ensure_ascii=False)
-    text = "This meal is "+food_category+". Please perform the following 3 tasks based on the image. \
-    Task 1 : Identifying and Quantifying Nutritional Values of Foods in the Image. Analyze up to 5 foods \
-    Task 2 : Summarizing Nutritional Values of All Foods Consumed. \
-    Task 3 : Evaluate the diet on a scale of A to E based on the balance of nutrients. \
-    Task 4 : Make a short review depeding on my data, "+user_data+" \
-    Please answer food in Korean. Answer only in JSON format without explanation. And answer without spaces or line breaks inside the JSON. Use single quotes instead of double quotes inside the JSON and ReviewMessage.\
-    This is an example. {'task1':[{'떡볶이':{'calories':380,'carbohydrate':86,'protein':10,'fat':4.4}},{'김치':{'calories':34,'carbohydrate':7.1,'protein':2.0,'fat':0.6}},{'콩나물무침':{'calories':45,'carbohydrate':8.6,'protein':3.6,'fat':0.2}},{'부추전':{'calories':174,'carbohydrate':14.2,'protein':5.6,'fat':10.8}},{'잡곡밥':{'calories':210,'carbohydrate':44,'protein':5,'fat':1.5}},{'된장국':{'calories':40,'carbohydrate':5.9,'protein':3.9,'fat':0.5}}],'task2':{'total':{'calories':883,'carbohydrate':165.8,'protein':30.1,'fat':18}},'task3':'E','task4':'This food is not good for you'}\
-    "
+    text = "Please perform the following tasks based on the image. \
+    Task 1 : Please check if there are any foods in the image. Please answer either yes or no. If there is no food, do not perform following task. \
+    Task 2 : Identifying and Quantifying Nutritional Values of Foods in the Image.  Summarizing Nutritional Values of All Foods Consumed.  \
+    Task 3 : Evaluate the foods on a scale of A to E based on the balance of nutrients. \
+    Task 4 : Make a short review depeding on my data : "+user_data+" and suggest other foods. \
+    Please answer food in Korean. Answer only in JSON format without explanation. And answer without spaces or line breaks inside the JSON. Use single quotes instead of double quotes inside the JSON and ReviewMessage. \
+    This is an example if There are any foods in the image - {'task1': 'yes','task2':{'total':{'calories':883,'carbohydrate':165.8,'protein':30.1,'fat':18}},'task3':'E','task4': {'ReviewMessage': review_message, 'suggestion': food suggestions}} \
+    This is an another example if there are no foods in the image - {'task1': 'no'}"
 
     payload = {
-    "model": "gpt-4-vision-preview",
+    "model": "gpt-4o",
     "messages": [
         {
         "role": "user",
@@ -82,18 +82,29 @@ async def meal_infer(item: MealInferItem):
     }
 
     gpt_response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    
+    print(gpt_response.json())
+    
+    if 'choices' not in gpt_response.json().keys() : 
+        raise HTTPException(status_code=500, detail="Server Error. Please check out API KEY")
+    if 'message' not in gpt_response.json()["choices"][0].keys() :
+        raise HTTPException(status_code=500, detail="Server Error. Please check out API KEY")
+    if 'content' not in gpt_response.json()["choices"][0]['message'].keys() : 
+        raise HTTPException(status_code=500, detail="Server Error. Please check out API KEY")
+        
     gpt_response_content = gpt_response.json()["choices"][0]['message']['content']
     gpt_response_content = gpt_response_content.replace("\'", "\"")
     gpt_response_content = json.loads(gpt_response_content)
     
-    print(gpt_response_content)
-    nutrients_detail = gpt_response_content['task1']
+    is_food = gpt_response_content['task1']
+    if is_food == "no" : 
+        raise HTTPException(status_code=400, detail="I can't find any food in the image.")
+    
     nutrients_total = gpt_response_content['task2']['total']
     rating = gpt_response_content['task3']
     review = gpt_response_content['task4']
     
     response_dict = {}
-    response_dict['meal_detail'] = nutrients_detail
     response_dict['meal_total'] = nutrients_total
     response_dict['rating'] = rating
     response_dict['review'] = review
