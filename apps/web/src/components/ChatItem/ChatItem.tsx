1b649825f3f5386d51f77d5c7bb611a21eedaa44
hypnotes <hayeongpark@korea.ac.kr>
2024-06-04 22:46:24 +0900

    feat: chat 관련 컴포넌트

--- apps/web/src/components/ChatItem/ChatItem.tsx
+++ apps/web/src/components/ChatItem/ChatItem.tsx
@@ -1,21 +1,57 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 interface ChatItemProps {
   me: boolean;
   message: string;
+  createdAt: string;
+  isSending?: boolean;
 }
-export function ChatItem({ me, message }: ChatItemProps) {
+
+const today = new Date();
+
+export function ChatItem({ me, message, createdAt, isSending }: ChatItemProps) {
+  const isToday = today.toDateString() === new Date(createdAt).toDateString();
+  const time = new Date(createdAt).toLocaleTimeString('ko-KR', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  // 5월 6일 오후 3:00
+  const date = new Date(createdAt).toLocaleString('ko-KR', {
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
   return (
-    <Box display="flex" justifyContent={me ? 'right' : 'left'}>
-      <Box
-        bgcolor={me ? 'primary.main' : 'secondary.main'}
-        color={me ? 'primary.contrastText' : 'secondary.contrastText'}
-        borderRadius={1}
-        padding={1}
-        margin={1}
-        maxWidth="70%"
-      >
-        {message}
+    <Box display="flex" margin={1} justifyContent={me ? 'right' : 'left'} mb={1}>
+      <Box maxWidth="70%">
+        <Box
+          bgcolor={me ? 'primary.main' : 'secondary.main'}
+          color={me ? 'primary.contrastText' : 'secondary.contrastText'}
+          borderRadius={1}
+          padding={1}
+        >
+          {message}
+        </Box>
+        <Box
+          display="flex"
+          justifyContent={me ? 'right' : 'left'}
+          ml={me ? '0' : '4px'}
+          mr={me ? '4px' : '0'}
+          mt={'2px'}
+        >
+          {!isSending ? (
+            <Typography variant="caption" fontSize={10}>
+              {isToday ? time : date}
+            </Typography>
+          ) : (
+            <Typography variant="caption" fontSize={10}>
+              전송중..
+            </Typography>
+          )}
+        </Box>
       </Box>
     </Box>
   );
---
+++ apps/web/src/components/ChatItem/LoadingChat.tsx
@@ -0,0 +1,24 @@
+import { Box } from '@mui/material';
+import LoadingLottie from '../../assets/lottie/chatLoad.json';
+import Lottie from 'react-lottie';
+
+const options = {
+  loop: true,
+  autoplay: true,
+  animationData: LoadingLottie,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+export function LoadingChatItem() {
+  return (
+    <Box display="flex" justifyContent={'left'}>
+      <Box maxWidth="70%">
+        <Box bgcolor={'secondary.main'} color={'secondary.contrastText'} borderRadius={1}>
+          <Lottie options={options} height={34} width={'80%'} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
