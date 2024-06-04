967c7eb9afb14ff09345070fca7eff524cc2830d
hypnotes <hayeongpark@korea.ac.kr>
2024-06-04 22:44:31 +0900

    feat: 에러 스낵바 두개 방지위해 enabled에 로그인 여부 포함

--- apps/web/src/apis/auth/_hooks/me.ts
+++ apps/web/src/apis/auth/_hooks/me.ts
@@ -3,14 +3,19 @@ import { QUERY_KEYS } from '../../../constants/queryKeys';
 import { getUserInfo } from '..';
 import { enqueueSnackbar } from 'notistack';
 import { useFlow } from '../../../layouts/stackflow';
+import { useSetUser } from '../../../recoil/auth';
 
 export const useGetUserInfo = () => {
   const { push } = useFlow();
+  const setUser = useSetUser();
 
   return useQuery({
     queryKey: QUERY_KEYS.ME,
     queryFn: () => getUserInfo(),
     options: {
+      onSuccess: data => {
+        setUser(data);
+      },
       onError: () => {
         enqueueSnackbar('비타에 로그인 해 주세요!', { variant: 'warning' });
         push('AuthActivity', {});
--- apps/web/src/apis/meal/_hooks/getMeal.ts
+++ apps/web/src/apis/meal/_hooks/getMeal.ts
@@ -1,10 +1,21 @@
 import { useQuery } from '../../../config/react-query/useQuery';
 import { QUERY_KEYS } from '../../../constants/queryKeys';
 import { getMealByDate } from '..';
+import { useUser } from '../../../recoil/auth';
+import useSaveTodayNutrient from '../../../activities/MainActivity/_hooks/useSaveTodayNutrient';
 
 export const useGetMealByDate = (date: string) => {
+  const user = useUser();
+  const { saveNutrient } = useSaveTodayNutrient();
+
   return useQuery({
     queryKey: [QUERY_KEYS.MEAL_BY_DATE, date],
     queryFn: () => getMealByDate(date),
+    options: {
+      enabled: !!user,
+      onSuccess: data => {
+        saveNutrient(data);
+      },
+    },
   });
 };
