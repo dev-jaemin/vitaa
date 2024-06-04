11104e1f100ceaf67a5e4c3675c4fa48ed592c68
Jaemin Kim <jay.kimjaemin@gmail.com>
2024-06-04 22:49:49 +0900

    feat: file attachment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
     exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
   });
   app.use(cookieParser());
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();
