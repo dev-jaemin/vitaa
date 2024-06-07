e53ed396df312766d0382dd03c7789def483497a
Jaemin Kim <jay.kimjaemin@gmail.com>
2024-06-07 22:35:16 +0900

    feat: adjust eslint-plugin-import

--- packages/config-eslint/index.js
+++ packages/config-eslint/index.js
@@ -2,9 +2,12 @@ module.exports = {
   env: {
     node: true,
   },
+  settings: {
+    'import/parsers': { '@typescript-eslint/parser': ['.ts'] },
+  },
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:import/typescript'],
+  plugins: ['@typescript-eslint', 'eslint-plugin-import'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
@@ -12,5 +15,24 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'builtin',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'react-dom'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
 };
