ce89c15d89bdee02ac9d89a39b63152267da8c9b
Jaemin Kim <jay.kimjaemin@gmail.com>
2024-06-08 20:50:59 +0900

    fix: console log 추가

--- apps/web/src/providers/AuthProvider.tsx
+++ apps/web/src/providers/AuthProvider.tsx
@@ -35,6 +35,9 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       setUser(userData);
     }
 
+    console.log('userData', userData);
+    console.log('isLoading', isLoading);
+
     if (!AUTH_WHITE_LIST.find(whitePath => pathname.startsWith(whitePath)) && !isLoading && !userData) {
       enqueueSnackbar('비타에 로그인 해 주세요!', { variant: 'warning' });
       push('AuthActivity', {});
